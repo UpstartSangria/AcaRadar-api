313b2aba51e6c4fca3a05255cb00700da2beae5a
aronvukatana <aronvukatana@gmail.com>
2025-12-08 10:34:37 +0800

    Environment fix for CI (test env)

--- config/environment.rb
+++ config/environment.rb
@@ -7,32 +7,47 @@ require 'sequel'
 require 'rack/session'
 
 module AcaRadar
-  # Configuration for the App
   class App < Roda
     plugin :environments
 
-    if ENV['RACK_ENV'] == 'development' || ENV['RACK_ENV'] == 'test' || ENV['RACK_ENV'].nil?
-      configure :development, :test do
-        Figaro.application = Figaro::Application.new(
-          environment:,
-          path: File.expand_path('config/secrets.yml') #changed from secrets_example.yml
-        )
-        Figaro.load
-        def self.config = Figaro.env
-        # use scecret set by the env, use fall back
-        use Rack::Session::Cookie, secret: ENV.fetch('SESSION_SECRET')
-        CONFIG = YAML.safe_load_file('config/secrets.yml') #changed from secrets_example.yml
-        ENV['DATABASE_URL'] ||= "sqlite://#{config.DB_FILENAME}"
-      end
+    # ---------------------------
+    # DEVELOPMENT ENVIRONMENT
+    # ---------------------------
+    configure :development do
+      Figaro.application = Figaro::Application.new(
+        environment:,
+        path: File.expand_path('config/secrets.yml')  # dev uses secrets.yml
+      )
+      Figaro.load
+
+      def self.config = Figaro.env
+
+      use Rack::Session::Cookie, secret: ENV.fetch('SESSION_SECRET')
+      CONFIG = YAML.safe_load_file('config/secrets.yml')
+      ENV['DATABASE_URL'] ||= "sqlite://#{config.DB_FILENAME}"
     end
 
-    configure :production do
-      CONFIG = YAML.safe_load_file('config/secrets_example.yml')
+    # ---------------------------
+    # TEST ENVIRONMENT  (CI)
+    # ---------------------------
+    configure :test do
+      # CI uses GitHub Action secrets
       def self.config = ENV
+
+      use Rack::Session::Cookie, secret: ENV.fetch('SESSION_SECRET')
+      ENV['DATABASE_URL'] ||= "sqlite://#{ENV['DB_FILENAME']}"
+    end
+
+    # ---------------------------
+    # PRODUCTION (Heroku)
+    # ---------------------------
+    configure :production do
+      def self.config = ENV
+
       use Rack::Session::Cookie, secret: ENV.fetch('SESSION_SECRET')
     end
 
     @db = Sequel.connect(ENV.fetch('DATABASE_URL'))
-    def self.db = @db # rubocop:disable Style/TrivialAccessors
+    def self.db = @db
   end
 end
